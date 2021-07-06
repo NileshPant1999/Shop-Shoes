3305990d70d43b96db0f5b474312b98ca2cc9811
Nilesh Pant <nileshpant6969@gmail.com>
2021-07-06 19:48:31 +0530

    basic setup

--- App.tsx
+++ App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Onboarding from './src/Authentication/Onboarding/Onboarding';
+import { LoadAssets } from './src/components';
+
+const fonts = {
+  "SFProText-Bold": require("./assets/fonts/SF-Pro-Text-Bold.otf"),
+  "SFProText-Semibold": require("./assets/fonts/SF-Pro-Text-Semibold.otf"),
+  "SFProText-Regular": require("./assets/fonts/SF-Pro-Text-Regular.otf"),
+};
+
+const AuthencationStack = createStackNavigator()
+
+const AuthencationNavigator = () => {
+  return (
+    <AuthencationStack.Navigator headerMode={'none'}>
+       <AuthencationStack.Screen name={"onboarding"} component={Onboarding} />
+    </AuthencationStack.Navigator>
+  )
+  
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LoadAssets {...{ fonts }}>
+      <AuthencationNavigator />
+    </LoadAssets>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'cyan',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { View, Text, StyleSheet, Dimensions } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import Slide from './Slide'
+
+const { width, height } = Dimensions.get("window")
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "white"
+    },
+    slider: {
+        height: 0.61 * height,
+        backgroundColor: "cyan",
+        borderBottomRightRadius: 75
+    },
+    footer: {
+        flex: 1,
+    }
+})
+
+export default function Onboarding() {
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.slider}>
+                <ScrollView horizontal snapToInterval={width} decelerationRate="fast" showsHorizontalScrollIndicator={false} bounces={false}>
+                    <Slide label="Relaxed" />
+                    <Slide label="Playful" right/>
+                    <Slide label="Ethnic"/>
+                    <Slide label="Funky" right/>
+                </ScrollView>
+            </View>
+            <View style={styles.footer}>
+                <View style={{ ...StyleSheet.absoluteFillObject, backgroundColor: "cyan"}} />
+                <View style={{flex: 1, backgroundColor: "white", borderTopLeftRadius: 75}}>
+
+                </View>
+            </View>  
+        </View>
+    )
+}
